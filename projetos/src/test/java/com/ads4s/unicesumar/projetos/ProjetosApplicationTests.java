c3c84074825c8bec43f9c6fec2ec254d3d92d5fe
Dione dos Santos <73508786+dione-is@users.noreply.github.com>
2021-10-24 22:46:56 -0300

    adicionando projeto maven

---
+++ projetos/src/test/java/com/ads4s/unicesumar/projetos/ProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ads4s.unicesumar.projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
